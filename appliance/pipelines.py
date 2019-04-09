c63e565681021f0c61d3d2de1c6c3682219643e2
Jahan <23203130+husnejahan@users.noreply.github.com>
2019-04-09 16:30:26 -0400

    Add files via upload

---
+++ appliance/pipelines.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+# Scraped data -> Item Containers -> Json/csv files
+# Scraped data -> Item Containers -> Pipeline -> SQL/Mongo database
+
+import mysql.connector
+
+
+class GeappliancePipeline(object):
+
+    def __init__(self):
+        self.create_connection()
+        self.create_table()
+
+    def create_connection(self):
+        self.conn = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='Jesma1423',
+            database='geappliance'
+        )
+        self.curr = self.conn.cursor()
+
+
+    def create_table(self):
+        self.curr.execute("""DROP TABLE IF EXISTS appliance_tb(""")
+        self.curr.execute("""create table appliance_tb(
+
+                                 product_name text,
+                                 product_code  text,
+                                 product_price  text,
+                                 product_imagelink text
+                                 )""")
+
+    def process_item(self, item, spider):
+        self.store_db(item)
+        return item
+
+    def store_db(self, item):
+        self.curr.execute("""insert into appliance_tb( values (%s,%s,%s,%s)""", (
+            items['product_name'][0],
+            items['product_code'][0],
+            items['product_price'][0],
+            items['product_imagelink'][0]
+        ))
+        self.conn.commit()
